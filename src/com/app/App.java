70aa8eafeb15bde98b78bfd4c56c72b2cfaef70d
Manuel E. Hernandez <kicke10h@gmail.com>
2021-09-17 16:03:52 -0500

    Add comments

--- src/com/app/App.java
+++ src/com/app/App.java
@@ -1,14 +1,15 @@
 package com.app;
 
 class App {
-	public static void main(String[] args) {
-		Thread intAsslep = new Thread_1();
+	public static void main(String[] args) { // The main thread 
+		Thread intAsslep = new Thread_1(); // Make the other thread 
 		try{
-			intAsslep.start();
-			Thread.sleep(2000);
+			//Execute the intAsslep thread for two seconds and then make a interruption 
+			intAsslep.start(); 
+			Thread.sleep(2000); 
 			intAsslep.interrupt();
 		}
-		catch(InterruptedException e){
+		catch(InterruptedException e){ // Catch a Interrupted Exception 
 			e.printStackTrace();
 		}
 	}
--- src/com/app/Thread_1.java
+++ src/com/app/Thread_1.java
@@ -2,13 +2,13 @@ package com.app;
 
 public class Thread_1 extends Thread {
 	@Override
-	public void run(){
-		while(true){
+	public void run(){ //Override the method run
+		while(true){ //Make the infinite loop
 			try{
 				System.out.println("Hello!");
 				Thread.sleep(100);
 			}
-			catch(InterruptedException e){
+			catch(InterruptedException e){ //Out when make a interruption
 				System.out.println("Interrupted");
 				break;
 			}
